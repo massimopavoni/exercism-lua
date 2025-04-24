a086b987eb4d71e5d1f870d293c6fc0e8f09f43c
Massimo Pavoni <maspavoni@gmail.com>
2025-04-24 08:58:23 +0200

    Fix last exercise not actually being completed

--- darts/darts.lua
+++ darts/darts.lua
@@ -1,6 +1,16 @@
 local Darts = {}
 
 function Darts.score(x, y)
+	local position = math.sqrt(x ^ 2 + y ^ 2)
+	if position <= 1 then
+		return 10
+	elseif position <= 5 then
+		return 5
+	elseif position <= 10 then
+		return 1
+	else
+		return 0
+	end
 end
 
 return Darts
